01768b6f7d1e1008b6ab2610269e9bc0e6b430b0
Younus Khan <younuskhan99@hotmail.com>
2022-05-25 16:38:52 +0100

    bug fixed

--- script.js
+++ script.js
@@ -2,22 +2,19 @@ import { pokemonArray } from './data/pokemon.js';
 
 const container = document.querySelector(".card-container");
 
-
-
-
-
-
 const displayCards = (object) => {
     return ( 
         `<div class="card">
             <img src="${object.sprite}" class="card__image">
             <div class="card__content">
-                <div class="card__heading">${object.name}</div>
-                <div class="card__text">${object.name} (#${object.id}) is a ${object.types.join(", ")} pokemon</div>
+                <div class="card__heading">${object.name[0].toUpperCase()+object.name.substring(1)}</div>
+                <div class="card__text">${object.name[0].toUpperCase()+object.name.substring(1)} (#${object.id}) is a ${object.types.join(", ")} pokemon.</div>
             </div>
         </div>`
     )
 };
+
+
 
 pokemonArray.forEach(pokemon => {
     container.innerHTML += displayCards(pokemon)
@@ -26,39 +23,46 @@ pokemonArray.forEach(pokemon => {
 // EXTENTION
 
 const h1 = document.querySelector("h1")
+h1.remove()
 
-h1.innerHTML += (`<br>
-                <br>
-                <label for="pokemonName">Search Pokèmon by Name:</label>
-                <input id="pokemonName" type="text">
+const body = document.querySelector("body")
 
-                <label for="pokemonType">Search Pokèmon by Type:</label>
-                <input id="pokemonType" type="text">
-                `)
+const inputSection = document.createElement("div")
+
+inputSection.innerHTML += `
+<h1>PokéDom- gotta code 'em all!</h1>
+<br>
+<br>
+<label for="pokemonName">Search Pokèmon:</label>
+<input id="pokemonName" type="text" style="height: 3vh">
+
+<label for="pokemonType">Search Pokèmon by Type:</label>
+<input id="pokemonType" type="text" style="height: 3vh">
+`
+body.insertBefore(inputSection, body.firstChild);
+
 
 const pokemonName = document.getElementById("pokemonName")
 const pokemonType = document.getElementById("pokemonType")
 
 const searchByName = (event) => {
-    
     container.innerHTML = "";
-    
     pokemonArray.forEach(pokemon => {
         if (pokemon.name.toLowerCase().includes(event.target.value.toLowerCase())) {
             container.innerHTML += displayCards(pokemon)
-        }
+        } 
     });
 };
 
 pokemonName.addEventListener("input", searchByName)
 
 const searchByType = (event) => {
-    
     container.innerHTML = "";
-    
     pokemonArray.forEach(pokemon => {
-        if (pokemon.types[0].toLowerCase().includes(event.target.value.toLowerCase()) || pokemon.types[1].toLowerCase().includes(event.target.value.toLowerCase())) {
-            container.innerHTML += displayCards(pokemon)
+        for (let i = 0; i < pokemon.types.length; i++) {
+            if (pokemon.types[i].includes(event.target.value.toLowerCase())) {
+                container.innerHTML += displayCards(pokemon)
+            }    
         }
     });
 };
